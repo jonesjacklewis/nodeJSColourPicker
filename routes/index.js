e8b297336dae0628333ce9ea6190cf5ccdd22768
jonesjacklewis <jones.jack.lewis@gmail.com>
2022-03-25 10:08:31 +0000

    eslint

--- routes/index.js
+++ routes/index.js
@@ -1,52 +1,57 @@
-var express = require("express");
-const axios = require("axios");
-var router = express.Router();
+const express = require('express');
+const axios = require('axios');
+// eslint-disable-next-line new-cap
+const router = express.Router();
+
+const invalidColourCode = `
+<script>
+window.alert('Invalid Colour Code');
+window.location.href='/'
+</script>`;
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", {
-    title: "Colour Picker",
-    method: "get",
+router.get('/', function(req, res, next) {
+  res.render('index', {
+    title: 'Colour Picker',
+    method: 'get',
   });
 });
 
-router.post("/", function (req, res, next) {
+router.post('/', function(req, res, next) {
   const requestBody = req.body;
 
-  if ("colourSelect" in requestBody) {
-    const colour = requestBody["colourSelect"];
-    const imgBaseURL = "https://www.thecolorapi.com/id?format=svg&hex=";
+  if ('colourSelect' in requestBody) {
+    const colour = requestBody['colourSelect'];
+    const imgBaseURL = 'https://www.thecolorapi.com/id?format=svg&hex=';
 
-    const imgURL = imgBaseURL + colour.replace("#", "");
+    const imgURL = imgBaseURL + colour.replace('#', '');
 
-    const requestBaseURL = "https://www.thecolorapi.com/id?hex=";
+    const requestBaseURL = 'https://www.thecolorapi.com/id?hex=';
 
-    const requestURL = requestBaseURL + colour.replace("#", "");
+    const requestURL = requestBaseURL + colour.replace('#', '');
 
     axios
-      .get(requestURL)
-      .then((data) => {
-        data = data["data"];
-        const name = data["name"]["value"];
-        const perfectMatch = data["name"]["exact_match_name"];
+        .get(requestURL)
+        .then((data) => {
+          data = data['data'];
+          const name = data['name']['value'];
+          const perfectMatch = data['name']['exact_match_name'];
 
-        res.render("index", {
-          title: "Colour Picker",
-          method: "post",
-          imgUrl: imgURL,
-          colourName: name,
-          perfectMatch: perfectMatch,
-          hex: colour,
+          res.render('index', {
+            title: 'Colour Picker',
+            method: 'post',
+            imgUrl: imgURL,
+            colourName: name,
+            perfectMatch: perfectMatch,
+            hex: colour,
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+          res.send(invalidColourCode);
         });
-      })
-      .catch((error) => {
-        console.log(error);
-        res.send(
-          "window.alert('Invalid Colour Code');window.location.href='/'"
-        );
-      });
   } else {
-    res.send("window.alert('Invalid Colour Code');window.location.href='/'");
+    res.send(invalidColourCode);
   }
 });
 
